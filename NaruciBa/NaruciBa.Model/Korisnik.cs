f75184dc17d46dd002c4f74de48df2c8713bab27
Velid Hatic <haticv92@gmail.com>
2021-12-23 14:29:54 +0100

    Added Model project library and AutoMapper tool

---
+++ NaruciBa/NaruciBa.Model/Korisnik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaruciBa.Model
+{
+    public partial class Korisnik
+    {
+        public int KorisnikID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Adresa { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public DateTime? DatumRodenja { get; set; }
+        public DateTime? DatumKreiranja { get; set; }
+        public DateTime? DatumIzmjene { get; set; }
+        public byte[] Slika { get; set; }
+        public int? GradID { get; set; }
+    }
+}
---
+++ NaruciBa/NaruciBa/Mapping/NaruciBaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NaruciBa.Mapping
+{
+    public class NaruciBaProfile : Profile
+    {
+        public NaruciBaProfile()
+        {
+            CreateMap<Database.Korisnik, Model.Korisnik>();
+        }
+    }
+}
